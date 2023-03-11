d2866435ebeb51e8aabf3b490048e73c53ff63e2
wenkey-everest <muni.v.g@everest.engineering>
2023-03-11 17:15:35 +0530

    Initial commit

---
+++ animal.py
@@ -0,0 +1,18 @@
+class Animal:
+    def eat(self):
+        print("I can Eat")
+
+    def sleep(self):
+        print("I can Sleep")
+
+    # def sound(self):
+    #     print("I can Sound")
+
+    # def run(self):
+    #     print("I can Run")
+
+    # def fly(self):
+    #     print("I can Fly")
+
+    # def swim(self):
+    #     print("I can Swim")
---
+++ main.py
@@ -0,0 +1,9 @@
+from dog import Dog
+
+
+class Main:
+    # Here DOG animal is created
+    Dog = Dog()
+    Dog.bark()
+    Dog.eat()
+    Dog.sleep()
